4cc2d9c17cb255838e9b6311070577283f8c5e48
Sebastian Monsalve <sebastiandev16@gmail.com>
2024-01-21 03:05:52 -0500

    add real profile and optimize other images

--- js/content.js
+++ js/content.js
@@ -133,7 +133,7 @@ function showContent(contentId) {
   } else if (contentId === "about") {
     var aboutData = [
       {
-        imageSrc: "/resources/5fa52569eb141.jpeg",
+        imageSrc: "/resources/Profile1.jpg",
         name: "Sebastian Monsalve",
         content:
           "Since I was a child, I immersed myself in the world of technology thanks to my dad, Who at that time was studying a basic technique on systems. Watching him in front of the PC doing his practical exercises fascinated me, so I started tinkering on my own. I even attempted to create a video game in Unity without any programming knowledge, just following tutorials to the letter. That failed attempt (as expected🤡) convinced me to dive headfirst into continuing to learn more and more about computer science🥳.",
@@ -141,7 +141,7 @@ function showContent(contentId) {
         postOptions: ["Like", "Dislike", "Comment"],
       },
       {
-        imageSrc: "/resources/5fa52569eb141.jpeg",
+        imageSrc: "/resources/Profile1.jpg",
         name: "Sebastian Monsalve",
         content:
           "I'm someone with a strong sense of belonging. I like to lend a hand wherever I can to ensure everything runs smoothly (perhaps because I don't like seeing things not working quite right—maybe I'm a bit of a perfectionist, just a bit... hahaha😅). On another note (as an additional piece of information that no one asked for😅), I've always been interested in the world of art and culture. In school, I won several first and second-place medals in troubadour singing and drawing (who would have thought? Now I can't even draw a stickman🤡😂).",
@@ -149,7 +149,7 @@ function showContent(contentId) {
         postOptions: ["Like", "Dislike", "Comment"],
       },
       {
-        imageSrc: "/resources/5fa52569eb141.jpeg",
+        imageSrc: "/resources/Profile1.jpg",
         name: "Sebastian Monsalve",
         content:
           "I'm not a big sports fanatic, but swimming has won me over. Although I haven't practiced it professionally for a while, I'm always ready to take a plunge when the mood strikes. And since we're talking about my interests, music is my constant companion. Not to mention cinema. I love watching movies and series, but not just for entertainment. I consider myself an expert critic (according to me and no one else hahahaha😅😂), analyzing everything from performances to soundtracks, from camera angles to color palettes. If something doesn't click for me, I swear, I won't buy into it. What can I say? An exceptional being🤭. Oh, and I also like taking photos📷.",
